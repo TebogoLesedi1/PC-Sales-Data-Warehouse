9d633a1135df360391c57d17402d17b3b3b4b264
TebogoLesedi1 <leseditebogo22@gmail.com>
2026-04-23 13:58:07 +0200

    adding a stored procedure file of fact_pc_sales table

---
+++ stored_procedure/sp_fact_pc_sales_table.sql
@@ -0,0 +1,29 @@
+----------stored_procedure_fact_pc_sales_table
+CREATE PROCEDURE sp_fact_pc_sales AS
+
+BEGIN
+DROP TABLE [Computer_std].[dbo].[fact_pc_sales];
+CREATE TABLE [Computer_std].[dbo].[fact_pc_sales](
+	[Sales_ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[Cost_Price] [smallint] NOT NULL,
+	[Sale_Price] [int] NOT NULL,
+	[Discount_Amount] [smallint] NOT NULL,
+	[Finance_Amount] [int] NOT NULL,
+	[Credit_Score] [smallint] NOT NULL,
+	[Cost_of_Repairs] [smallint] NOT NULL,
+	[Total_Sales_per_Employee] [smallint] NOT NULL,
+	[PC_Market_Price] [int] NOT NULL
+)
+INSERT INTO [Computer_std].[dbo].[fact_pc_sales](Cost_Price,Sale_Price,Discount_Amount,Finance_Amount,
+Credit_Score,Cost_of_Repairs, Total_Sales_per_Employee, PC_Market_Price)
+      SELECT DISTINCT
+       [Cost_Price]
+      ,[Sale_Price]
+      ,[Discount_Amount]
+      ,[Finance_Amount]
+      ,[Credit_Score]
+      ,[Cost_of_Repairs]
+      ,[Total_Sales_per_Employee]
+      ,[PC_Market_Price]
+  FROM [Computer_std].[dbo].[raw_pc_data];
+END;
