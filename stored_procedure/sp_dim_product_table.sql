f106d5be0fb65a81943eabaee68006a0df7773dc
TebogoLesedi1 <leseditebogo22@gmail.com>
2026-04-23 12:36:38 +0200

    adding stored procedure file on dim_product file

---
+++ stored_procedure/sp_dim_product_table.sql
@@ -0,0 +1,23 @@
+----------stored_procedure_dim_product_table
+CREATE PROCEDURE sp_dim_product AS
+
+BEGIN
+DROP TABLE [Computer_std].[dbo].[dim_product];
+CREATE TABLE [Computer_std].[dbo].[dim_product](
+	[Product_ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[PC_Make] [nvarchar](50) NOT NULL,
+	[PC_Model] [nvarchar](50) NOT NULL,
+	[Storage_Type] [nvarchar](50) NOT NULL,
+	[RAM] [nvarchar](50) NOT NULL,
+	[Storage_Capacity] [nvarchar](50) NOT NULL
+)
+INSERT INTO [Computer_std].[dbo].[dim_product](PC_Make, PC_Model, Storage_Type, RAM,Storage_Capacity)
+    SELECT DISTINCT
+       [PC_Make]
+      ,[PC_Model]
+      ,[Storage_Type]
+      ,[RAM]
+      ,[Storage_Capacity]
+  FROM [Computer_std].[dbo].[raw_pc_data];
+
+END;
